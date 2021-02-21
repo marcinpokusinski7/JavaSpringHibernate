7e85f5ac1db776f7790c11ce983373e70e9a239b
Marcin <marcinpokusinski@gmail.com>
2021-02-21 10:33:10 +0100

    deleting db record

--- src/main/java/com/example/demo/hibernate/DeleteCourseAndReviewsDemo.java
+++ src/main/java/com/example/demo/hibernate/DeleteCourseAndReviewsDemo.java
@@ -8,7 +8,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class GetCourseAndReviewsDemo {
+public class DeleteCourseAndReviewsDemo {
     public static void main(String[] args) {
 
         // create session factory
@@ -30,9 +30,11 @@ public class GetCourseAndReviewsDemo {
             int theId = 10;
             Course tempCourse = session.get(Course.class, theId);
             // print the course
-            System.out.println(tempCourse);//lazy fetch only course
+            System.out.println("Deleting the course: ");//lazy fetch only course
             // print the course reviews
             System.out.println(tempCourse.getReviews()); // lazy fetch course, and reviews
+            // delete the course
+            session.delete(tempCourse);
             // commit transaction
             session.getTransaction().commit();
             System.out.println("Done!");
